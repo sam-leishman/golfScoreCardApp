125c805fa3c6ef5b4237fafd8ca7ef4f164bda3f
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-11-03 10:22:30 -0700

    Add courses page

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
+/*
 const httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        const golfInfo = JSON.parse(httpRequest.responseText)
-        console.log(golfInfo);
+        const courses = JSON.parse(httpRequest.responseText)
+        console.log(courses);
     }
 }
 httpRequest.open('GET', 'https://golf-courses-api.herokuapp.com/courses', true);
 httpRequest.send();
+*/
+
+// /*
+// Grabs the courses
+let coursesPromise = fetch('https://golf-courses-api.herokuapp.com/courses')
+    .then((response) => response.json());
+coursesPromise
+    .then((data) => printInfo(data));
+// */
+
+function getCourse(courseId) {
+    return fetch(`https://golf-courses-api.herokuapp.com/courses/${courseId}`)
+        .then((response) => response.json());
+}
