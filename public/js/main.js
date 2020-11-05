878fbf3731773c5a03934c421fc1e5f4b08a4f1c
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-11-05 08:25:25 -0700

    Add page navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+const foxHollowCourse = document.querySelector('[data-fox-hollow]')
+const thanksgivingPointCourse = document.querySelector('[data-thanksgiving-point]')
+const spanishOaksCourse = document.querySelector('[data-spanish-oaks]')
+
+
+
 /*
 const httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = () => {
@@ -15,10 +21,24 @@ httpRequest.send();
 let coursesPromise = fetch('https://golf-courses-api.herokuapp.com/courses')
     .then((response) => response.json());
 coursesPromise
-    .then((data) => printInfo(data));
+    .then((data) => console.log(data));
 // */
 
 function getCourse(courseId) {
     return fetch(`https://golf-courses-api.herokuapp.com/courses/${courseId}`)
-        .then((response) => response.json());
+        .then((response) => response.json())
+        .then((data) => console.log(data.data));
 }
+
+foxHollowCourse.addEventListener('click', e => {
+    getCourse(18300);
+    window.location = './index.html'
+})
+thanksgivingPointCourse.addEventListener('click', e => {
+    getCourse(11819);
+    window.location = './index.html'
+})
+spanishOaksCourse.addEventListener('click', e => {
+    getCourse(19002);
+    window.location = './index.html'
+})
