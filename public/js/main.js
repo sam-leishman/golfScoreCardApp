c3b2b89a716452411abc24b416f10fa86df465d2
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-11-19 08:17:14 -0700

    Correctly print data entered to scorecard

--- public/js/main.js
+++ public/js/main.js
@@ -24,14 +24,13 @@ httpRequest.send();
 
 // /*
 // Grabs the courses
-let coursesPromise = fetch('https://golf-courses-api.herokuapp.com/courses')
+let allCoursesPromise = fetch('https://golf-courses-api.herokuapp.com/courses')
     .then((response) => response.json());
-coursesPromise
+allCoursesPromise
     .then((data) => populateCourses(data.courses));
 // */
 
 let coursePromise;
-
 function getCourse(courseId) {
     coursePromise = fetch(`https://golf-courses-api.herokuapp.com/courses/${courseId}`)
         .then((response) => response.json());
@@ -41,7 +40,7 @@ function getCourse(courseId) {
 }
 
 function populateCourses(courses) {
-    let html = '';
+    let html = `<option></option>`;
     courses.forEach(course => {
         html += `
             <option value="${course.id}">${course.name}</option>
@@ -50,341 +49,201 @@ function populateCourses(courses) {
     document.getElementById('courses').innerHTML = html;
 }
 
+function checkHoles() {
+    if (selectedHoles.value == '9') {
+        console.log('nine')
+        $('#topRow').append('<td class="totalCell">TOTAL</td>')
+        $('#uptonine > tbody > tr:not(#topRow)').append('<td class="totalCell"></td>')
+    }
+    if (selectedHoles.value == '18') {
+        console.log('eighteen')
+        document.getElementById('scorecard').innerHTML += `
+        <table id="uptoeighteen">
+            <tr>
+                <th>Hole</th>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>IN</td>
+                <!-- If 18 holes are chosen -->
+                <td class="totalCell">TOTAL</td>
+            </tr>
+            <tr id="cardTeeboxes2">
+                <th>Yards</th>
+                <td id="yards10"></td>
+                <td id="yards11"></td>
+                <td id="yards12"></td>
+                <td id="yards13"></td>
+                <td id="yards14"></td>
+                <td id="yards15"></td>
+                <td id="yards16"></td>
+                <td id="yards17"></td>
+                <td id="yards18"></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr>
+                <th>Handicap</th>
+                <td id="hcp10"></td>
+                <td id="hcp11"></td>
+                <td id="hcp12"></td>
+                <td id="hcp13"></td>
+                <td id="hcp14"></td>
+                <td id="hcp15"></td>
+                <td id="hcp16"></td>
+                <td id="hcp17"></td>
+                <td id="hcp18"></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr id="player1Row2">
+                <td id="player1Name2">player1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr id="player2Row2">
+                <td id="player2Name2">player2</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr id="player3Row2">
+                <td id="player3Name2">player3</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr id="player4Row2">
+                <td id="player4Name2">player4</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+            <tr>
+                <th>Par</th>
+                <td id="par10"></td>
+                <td id="par11"></td>
+                <td id="par12"></td>
+                <td id="par13"></td>
+                <td id="par14"></td>
+                <td id="par15"></td>
+                <td id="par16"></td>
+                <td id="par17"></td>
+                <td id="par18"></td>
+                <td></td>
+                <td class="totalCell"></td>
+            </tr>
+        </table>
+        `;
+        if (player1Name.value != '') {
+            document.getElementById('player1Name2').innerText = player1Name.value;
+        } else {
+            $('#player1Row2').remove();
+        }
+        if (player2Name.value != '') {
+            document.getElementById('player2Name2').innerText = player2Name.value;
+        } else {
+            $('#player2Row2').remove();
+        }
+        if (player3Name.value != '') {
+            document.getElementById('player3Name2').innerText = player3Name.value;
+        } else {
+            $('#player3Row2').remove();
+        }
+        if (player4Name.value != '') {
+            document.getElementById('player4Name2').innerText = player4Name.value;
+        } else {
+            $('#player4Row2').remove();
+        }
+    }
+}
+
 function populateTeeboxes(teeboxes) {
-    let teeboxHtml = '';
+    let teeboxHtml = `<option></option>`;
     teeboxes.forEach((teebox, index) => {
         teeboxHtml += `
             <option value="${index}">${teebox.teeType}</option>
         `;
     })
+    // console.log(teeboxes[0].par) // Par
     document.getElementById('teeboxes').innerHTML = teeboxHtml;
 }
 
-function populatePar(teeboxIndex) {
+function populateCard(teeboxIndex, numberOfHoles) {
     coursePromise
         .then((data) => {
-            console.log("Here: ", data, teeboxIndex)
+            console.log(numberOfHoles)
+            for (let i = 0; i < numberOfHoles; i++) {
+                const currentHole = data.data.holes[i];
+                const holeTeebox = currentHole.teeBoxes[teeboxIndex];
+                console.log(holeTeebox)
+                const holePar = holeTeebox.par;
+                const holeYards = holeTeebox.yards;
+                const holeHcp = holeTeebox.hcp;
+                document.getElementById(`par${currentHole.hole}`).innerText = holePar;
+                document.getElementById(`yards${currentHole.hole}`).innerText = holeYards;
+                document.getElementById(`hcp${currentHole.hole}`).innerText = holeHcp;
+            }
+            console.log("Here: ", data.data.holes, teeboxIndex)
+            // const teeColor = data.data.holes[0].teeboxes[teeboxIndex].teeHexColor
+            // document.getElementById('cardTeeboxes').style.backgroundColor = teeColor;
         })
 }
 
 submitParamsButton.addEventListener('click', () => {
 
-    let teeNumber = 0;
-
     if (player1Name.value == '' && player2Name.value == '' && player3Name.value == '' && player4Name.value == '') {
         console.log('No players entered??!1?!!1!')
     } else {
 
-        if (selectedTeebox.value == 'black') {
-            console.log('black')
-            document.getElementById('cardTeeboxes').innerHTML = `
-                <th>Black</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            `
-            document.getElementById('cardTeeboxes').classList.add('black');
-            teeNumber = 0;
-        }
-        if (selectedTeebox.value == 'blue') {
-            console.log('blue')
-            document.getElementById('cardTeeboxes').innerHTML = `
-                <th>Blue</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            `
-            document.getElementById('cardTeeboxes').classList.add('blue');
-            teeNumber = 1;
-        }
-        if (selectedTeebox.value == 'white') {
-            console.log('white')
-            document.getElementById('cardTeeboxes').innerHTML = `
-                <th>White</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            `
-            document.getElementById('cardTeeboxes').classList.add('white');
-            teeNumber = 2;
-        }
-        if (selectedTeebox.value == 'red') {
-            console.log('red')
-            document.getElementById('cardTeeboxes').innerHTML = `
-                <th>Red</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            `
-            document.getElementById('cardTeeboxes').classList.add('red');
-            teeNumber = 3;
+        let numberHoles = 0;
+        if (selectedHoles.value == 9) {
+            numberHoles = 9
+        } else if (selectedHoles.value == 18) {
+            numberHoles = 18
         }
 
-        if (selectedCourse.value == 'foxHollow') {
-            console.log('fox')
-            getCourse(18300)
-            let par1 = getPar(18300, 1, teeNumber);
-            document.getElementById('par1').innerText = par1;
-            document.getElementById('courseName').innerText = 'Fox Hollow';
-        }
-        if (selectedCourse.value == 'thanksgivingPoint') {
-            console.log('thank')
-            getCourse(11819)
-            document.getElementById('courseName').innerText = 'Thanksgiving Point';
-        }
-        if (selectedCourse.value == 'spanishOaks') {
-            console.log('spain')
-            getCourse(19002)
-            document.getElementById('courseName').innerText = 'Spanish Oaks';
-        }
-    
-        if (selectedHoles.value == '9') {
-            console.log('nine')
-            $('#topRow').append('<td class="totalCell">TOTAL</td>')
-            $('#uptonine > tbody > tr:not(#topRow)').append('<td class="totalCell"></td>')
-        }
-        if (selectedHoles.value == '18') {
-            console.log('eighteen')
-            document.getElementById('scorecard').innerHTML += `
-            <table id="uptoeighteen">
-                <tr>
-                    <th>Hole</th>
-                    <td>10</td>
-                    <td>11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>IN</td>
-                    <!-- If 18 holes are chosen -->
-                    <td class="totalCell">TOTAL</td>
-                </tr>
-                <tr id="cardTeeboxes2">
-                    <th>Color</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr>
-                    <th>Handicap</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr id="player1Row2">
-                    <td id="player1Name2">player1</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr id="player2Row2">
-                    <td id="player2Name2">player2</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr id="player3Row2">
-                    <td id="player3Name2">player3</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr id="player4Row2">
-                    <td id="player4Name2">player4</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-                <tr>
-                    <th>Par</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                </tr>
-            </table>
-            `;
-            if (selectedTeebox.value == 'black') {
-                console.log('black')
-                document.getElementById('cardTeeboxes2').innerHTML = `
-                    <th>Black</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                `
-                document.getElementById('cardTeeboxes2').classList.add('black');
-        
-            }
-            if (selectedTeebox.value == 'blue') {
-                console.log('blue')
-                document.getElementById('cardTeeboxes2').innerHTML = `
-                    <th>Blue</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                `
-                document.getElementById('cardTeeboxes2').classList.add('blue');
-            }
-            if (selectedTeebox.value == 'white') {
-                console.log('white')
-                document.getElementById('cardTeeboxes2').innerHTML = `
-                    <th>White</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                `
-                document.getElementById('cardTeeboxes2').classList.add('white');
-            }
-            if (selectedTeebox.value == 'red') {
-                console.log('red')
-                document.getElementById('cardTeeboxes2').innerHTML = `
-                    <th>Red</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="totalCell"></td>
-                `
-                document.getElementById('cardTeeboxes2').classList.add('red');
-            }
-
-            if (player1Name.value != '') {
-                document.getElementById('player1Name2').innerText = player1Name.value;
-            } else {
-                $('#player1Row2').remove();
-            }
-            if (player2Name.value != '') {
-                document.getElementById('player2Name2').innerText = player2Name.value;
-            } else {
-                $('#player2Row2').remove();
-            }
-            if (player3Name.value != '') {
-                document.getElementById('player3Name2').innerText = player3Name.value;
-            } else {
-                $('#player3Row2').remove();
-            }
-            if (player4Name.value != '') {
-                document.getElementById('player4Name2').innerText = player4Name.value;
-            } else {
-                $('#player4Row2').remove();
-            }
-        }
+        checkHoles();
+        populateCard(selectedTeebox.value, numberHoles);
     
         if (player1Name.value != '') {
             document.getElementById('player1Name').innerText = player1Name.value;
